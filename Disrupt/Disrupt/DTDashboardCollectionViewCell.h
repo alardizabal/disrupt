804baf35a08ffb6a0a03e667c55e7b61d65da160
Kevin Gray <Kevin@GoPosse.com>
2015-05-02 14:34:10 -0400

    Added a cell

---
+++ Disrupt/Disrupt/DTDashboardCollectionViewCell.h
@@ -0,0 +1,13 @@
+//
+//  DTDashboardCollectionViewCell.h
+//  Disrupt
+//
+//  Created by Kevin Gray on 5/2/15.
+//  Copyright (c) 2015 Posse. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DTDashboardCollectionViewCell : NSObject
+
+@end
---
+++ Disrupt/Disrupt/DTDashboardCollectionViewCell.m
@@ -0,0 +1,13 @@
+//
+//  DTDashboardCollectionViewCell.m
+//  Disrupt
+//
+//  Created by Kevin Gray on 5/2/15.
+//  Copyright (c) 2015 Posse. All rights reserved.
+//
+
+#import "DTDashboardCollectionViewCell.h"
+
+@implementation DTDashboardCollectionViewCell
+
+@end
