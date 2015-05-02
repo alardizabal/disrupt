52ca5405b97f2e26d497d2bea55ac6e6ecffd2a8
Rich McAteer <rmcateer1@gmail.com>
2015-05-02 11:59:16 -0700

    Adding reveal, DTCreateProjectViewController, and cell

---
+++ Disrupt/Disrupt/View Controllers/DTCreateProjectViewController.h
@@ -0,0 +1,13 @@
+//
+//  DTCreateProjectViewController.h
+//  Disrupt
+//
+//  Created by Rich McAteer on 5/2/15.
+//  Copyright (c) 2015 Posse. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DTCreateProjectViewController : UIViewController
+
+@end
